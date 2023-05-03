1e50f83d212034f5df23904eb4d4069d288d4619
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-05-03 19:38:02 +0530

    replace buttons with empty string

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,6 @@ function App() {
     function modifyDOM() {
       //You can play with your DOM here or check URL against your regex
       const documentCopy = document;
-      const elems = documentCopy.querySelectorAll("span button");
-      elems.forEach((e) => e.remove());
       return documentCopy.body.innerHTML;
     }
 
@@ -35,7 +33,9 @@ function App() {
       (results) => {
         //Here we have just the innerHTML and not DOM structure
         var div = document.createElement("div");
-        div.innerHTML = results[0].trim();
+        const buttonElem ='<button class="flex ml-auto gap-2"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>Copy code</button>'
+
+        div.innerHTML = results[0].trim().replace(buttonElem,'');;
 
         const chatTitleElem = div.getElementsByClassName(
           "flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all )} pr-14 )} bg-gray-800 hover:bg-gray-800 group"
